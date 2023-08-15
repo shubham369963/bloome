64b49b3083c41282d1652491dacbcf84a173a0b0
shubham369963 <shubhamhalade369@gmail.com>
2023-08-15 13:37:13 +0530

    backend implementation of authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,16 +1,117 @@
 const User = require('../model/userModel.js');
 const asyncHandler = require('express-async-handler');
 
-const getAllUsers = asyncHandler(async(req, res)=>{
 
-})
+const getAllUsers = asyncHandler(async (req, res) => {
+  try {
+    const users = await User.find({});
 
-const registerController = asyncHandler(async(req, res)=>{
-    
-})
+    return res.status(200).send({
+      userCount: users.length,
+      message: 'All Users Data',
+      success: true,
+      users,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: 'Error In Get All Users',
+      success: false,
+      error,
+    });
+  }
+});
 
-const loginController = asyncHandler(async(req, res)=>{
+const registerController = asyncHandler(async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-})
+    if (!username || !email || !password) {
+      return res.status(400).send({
+        message: 'Please Fill All Details',
+        success: false,
+      });
+    }
 
-module.exports = {getAllUsers, registerController, loginController}
+    const isExist = await User.findOne({ email });
+
+    if (isExist) {
+      // res.status(401);
+      // throw new Error('User Already Exists');
+      return res.status(401).send({
+        message: 'User Already Exists',
+        success: false,
+      });
+    }
+
+    const user = await User.create({ username, email, password });
+
+    if (user) {
+      return res.status(500).send({
+        message: 'New User Successfully Created',
+        success: true,
+        user,
+      });
+    } else {
+      // res.status(400);
+      // throw new Error('Error Occured');
+      return res.status(400).send({
+        message: 'Error Occured',
+        success: false,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: 'Error In Register',
+      success: false,
+      error,
+    });
+  }
+});
+
+const loginController = asyncHandler(async (req, res) => {
+  try {
+    const {email , password} = req.body;
+
+    if (!email || !password) {
+      return res.status(401).send({
+        message: 'Please Fill All Details',
+        success: false,
+      });
+    }
+
+    const user = await User.findOne({email});
+    if(!user){
+      return res.status(200).send({
+        message: 'User Not Registered',
+        success: false,
+      });
+    }
+
+    const isMatch = await user.matchPassword(password);
+
+    if(!isMatch){
+      return res.status(401).send({
+        message: 'Invalid Credentials',
+        success: false,
+      });
+    }
+
+    return res.status(200).send({
+      success: true,
+      message: "Login Successfull",
+      user,
+    })
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: 'Error In Login',
+      success: false,
+      error,
+    });
+  }
+});
+
+module.exports = { getAllUsers, registerController, loginController };
--- model/userModel.js
+++ model/userModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-
+const bcrypt = require("bcrypt");
 
 const userSchema = mongoose.Schema({
     username:{
@@ -17,7 +17,20 @@ const userSchema = mongoose.Schema({
 },
 {
     timestamps: true,
-})
+});
+
+userSchema.pre("save", async function (next){
+    if(!this.isModified("password")){
+        next();
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+
+userSchema.methods.matchPassword = async function (enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password);
+}
 
 const userModel = mongoose.model("User", userSchema);
 
